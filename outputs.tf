fbeb3af552978515d183c421924808b6da2f5341
Karlslin <nils.bamler@gmail.com>
2026-02-12 16:16:59 +0100

    added useful terraform outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
+  description = "Public IP address of the VM"
+  value       = azurerm_public_ip.pip.ip_address
+}
+
+output "website_url" {
+  description = "URL to access the nginx website"
+  value       = "http://${azurerm_public_ip.pip.ip_address}"
+}
+
+output "ssh_command" {
+  description = "SSH command to connect to the VM (Windows path)"
+  value       = "ssh -i C:\\Users\\nilsb\\.ssh\\id_rsa ${var.admin_username}@${azurerm_public_ip.pip.ip_address}"
 }
